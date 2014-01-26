bd8dfc802606c2102377c0d8f92c9019e5b78518
Jason Robinson <mail@jasonrobinson.me>
2014-01-26 21:28:35 +0200

    Add basic styling. Add masonry js library

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
+from django.conf.urls.static import static
+from django.conf import settings
+
 from dashboard.views import ContainerListView
 
 urlpatterns = patterns('',
     url(r'^$', ContainerListView.as_view()),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
